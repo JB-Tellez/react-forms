a2ee6cb2925a30deea30042a74a35341c10c5d52
JB Tellez <jb.tellez@gmail.com>
2023-11-29 21:31:18 -0800

    adds demo components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Header from './Header.jsx';
+
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [username, setUsername] = useState('user');
+
+  function handleFormSubmitted(event) {
+    event.preventDefault();
+    console.log('submitted');
+  }
+
+  function handleUsernameTyped(event) {
+    if (event.target.value.length > 0) {
+      setUsername(event.target.value);
+    } else {
+      setUsername('user');
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header user={username} />
+      <form onSubmit={handleFormSubmitted}>
+        <label htmlFor="name">Name</label>
+
+        <input id="name" name="name" type="text" onInput={handleUsernameTyped} />
+        <br />
+        <label htmlFor="dob">Date of Birth</label>
+        <input type="date" name="dob" id="dob" />
+        <br />
+        <input type="submit" />
+      </form>
+      <Container>
+        <Form>
+          <Form.Group controlId="bootstrapName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" onInput={handleUsernameTyped} />
+          </Form.Group>
+        </Form>
+      </Container>
+    </div>
   )
 }
 
-export default App
+export default App;
